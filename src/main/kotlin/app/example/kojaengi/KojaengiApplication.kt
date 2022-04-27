9f42430f645af013941bdc0ce644d8b63935a910
johnjihwan <s20062@gsm.hs.kr>
2022-04-27 18:24:58 +0900

    Add initial commit

---
+++ src/main/kotlin/app/example/kojaengi/KojaengiApplication.kt
@@ -0,0 +1,11 @@
+package app.example.kojaengi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KojaengiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KojaengiApplication>(*args)
+}
---
+++ src/test/kotlin/app/example/kojaengi/KojaengiApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.example.kojaengi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KojaengiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
