9fb02c31795af7ff8a4059bdcdce07328bc789a5
johnjihwan <s20062@gsm.hs.kr>
2022-04-29 11:01:23 +0900

    Add enum과 문자열 템플릿을 사용해봤다 프로퍼티 접근 수준도 알게되었다

--- src/test/kotlin/app/example/kojaengi/_02_코틀린_기초/enum/MakeColor.kt
+++ src/test/kotlin/app/example/kojaengi/_02_코틀린_기초/enum/MakeColor.kt
@@ -1,4 +1,23 @@
 package app.example.kojaengi._02_코틀린_기초.enum
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class MakeColor {
+
+    @Test
+    fun useColorTest(){
+        assertEquals("#F1024", ColorType.ORANGE.code)
+        assertEquals("orange", ColorType.ORANGE.colorName)
+    }
+
+    /**
+     * enum 에 생성자와 프로퍼티를 만들고 접근했다.
+     * @see ColorType.sayYourColor 문자열 템플릿을 사용했다.
+     */
+    @Test
+    fun viewSayColorTest(){
+        println(ColorType.ORANGE.sayYourColor())
+        println(ColorType.BROKEN_GREEN.sayYourColor())
+    }
 }
