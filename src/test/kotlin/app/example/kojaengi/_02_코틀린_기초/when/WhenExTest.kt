7acf388a5bfc49d884130a39bde8744015c85f5a
johnjihwan <s20062@gsm.hs.kr>
2022-04-29 18:04:35 +0900

    Add 굉장히 객체지향적이면서 함수형 프로그래밍이다 혼란스럽다

--- src/test/kotlin/app/example/kojaengi/_02_코틀린_기초/when/WhenExTest.kt
+++ src/test/kotlin/app/example/kojaengi/_02_코틀린_기초/when/WhenExTest.kt
@@ -1,4 +1,26 @@
 package app.example.kojaengi._02_코틀린_기초.`when`
 
+import org.junit.jupiter.api.Test
+
 class WhenExTest {
+
+    @Test
+    fun isSatisfyTest(){
+        val myCash = 10000
+        val yourCash = 2000
+
+        println(wealthAssessment(myCash))
+        println(wealthAssessment(yourCash))
+    }
+
+    /**
+     * Kotlin 에서의 When 개념을 적용시켰고.
+     * 범위 이터레이션 (in, ..) 을 사용하였다.
+     */
+    private fun wealthAssessment(cash: Int) = when{
+        cash in 1000..9999 -> "ordinary person"
+        10000 <= cash -> "gold spoon !"
+
+        else -> {"undefined"}
+    }
 }
--- src/test/kotlin/app/example/kojaengi/_02_코틀린_기초/when/tesla/ModelX.kt
+++
@@ -1,4 +0,0 @@
-package app.example.kojaengi._02_코틀린_기초.`when`.tesla
-
-class ModelX {
-}
