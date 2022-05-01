baa9b31aaca69582dd0fa4efdc8b8f2daefbf433
johnjihwan <s20062@gsm.hs.kr>
2022-05-01 17:15:29 +0900

    Add 모든 키워드를 사용해서 하는 방법

--- src/test/kotlin/app/example/kojaengi/_04_클래스_객체_인터페이스/주_생성자와_초기화_블록/Tesla.kt
+++ src/test/kotlin/app/example/kojaengi/_04_클래스_객체_인터페이스/주_생성자와_초기화_블록/Tesla.kt
@@ -1,5 +1,12 @@
 package app.example.kojaengi._04_클래스_객체_인터페이스.주_생성자와_초기화_블록
 
+class Car constructor(carTypes: String){
+    private val carType: String
+    init {
+        carType = carTypes
+    }
+}
+
 open class Tesla(val domainName: String)
 
 class Teslam(ownerName: String){
