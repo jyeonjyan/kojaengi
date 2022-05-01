6264f8f5b4948312e73df1bcdfa9a9345e6aec5b
johnjihwan <s20062@gsm.hs.kr>
2022-05-01 13:50:20 +0900

    Add 확장함수 :: refactoring 할 수 있다. 혁신적인지는 모르겟음

--- src/test/kotlin/app/example/kojaengi/_03_함수_정의와_호출/localFunExtention/UserServiceTest.kt
+++ src/test/kotlin/app/example/kojaengi/_03_함수_정의와_호출/localFunExtention/UserServiceTest.kt
@@ -1,4 +1,34 @@
 package app.example.kojaengi._03_함수_정의와_호출.localFunExtention
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+
 class UserServiceTest {
+
+    @Test
+    @DisplayName("확장 함수로 validate 로직을 빼서 DRY 원칙을 만족시킴")
+    fun userSave() {
+        val user = User("jyeonjyan")
+
+        assertThrows<IllegalArgumentException> { user.validate() }
+        // save user
+    }
+
+}
+
+class User(val name: String? = null, val age: Int? = null)
+
+/**
+ * 확장 함수를 사용해서 더 쉽게 코드를 파악할 수 있도록 함.
+ */
+private fun User.validate() {
+
+    if (name.isNullOrEmpty()) {
+        throw IllegalArgumentException("user의 name이 비어있어 처리할 수 없음. name: $name")
+    } else if (age == null) {
+        throw IllegalArgumentException("user의 name이 비어있어 처리할 수 없음. age: $age")
+    }
 }
