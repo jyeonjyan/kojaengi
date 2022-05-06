b9f6464fef19f371ef7bc938d9ed14ddf1e1f473
johnjihwan <s20062@gsm.hs.kr>
2022-05-06 23:19:19 +0900

    Add 멤버 참조 문법

---
+++ src/test/kotlin/app/example/kojaengi/_05_람다로_프로그래밍/_멤버_참조/Test.kt
@@ -0,0 +1,20 @@
+package app.example.kojaengi._05_람다로_프로그래밍._멤버_참조
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class Test {
+
+    @Test
+    @DisplayName("멤버 참조 문법:: 그림 5.2")
+    fun memberReferenceSyntax(){
+        val user1 = User("blackListUser")
+        val user2 = User("whiteListUser")
+
+        val userList = listOf(user1, user2)
+        val findUser = userList.filter { targetUser: User -> targetUser.name.equals("whiteListUser") }
+
+        assertEquals(user2, findUser.getOrNull(0))
+    }
+}
---
+++ src/test/kotlin/app/example/kojaengi/_05_람다로_프로그래밍/_멤버_참조/User.kt
@@ -0,0 +1,6 @@
+package app.example.kojaengi._05_람다로_프로그래밍._멤버_참조
+
+/**
+ * name이 nullable한 객체면 .equals 를 사용할 수 없다.
+ */
+class User(val name: String? = "anonymous")
